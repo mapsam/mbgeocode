23d4d8961b1db8cf87ee25943cd95abbfff1cfff
mapsam <matthews.sam@gmail.com>
2015-12-20 13:01:27 -0800

    clean test

--- test/test.js
+++ test/test.js
@@ -4,20 +4,6 @@ var test = require('tap').test;
 var mbgeocode = require('../index.js');
 
 test('csv > geojson', function (t) {
-  // vt2geojson({
-  //     url: 'http://api.mapbox.com/9/150/194.pbf',
-  //     layer: 'state_label'
-  // }, function (err, result) {
-  //     t.ifError(err);
-  //     t.deepEqual(result.type, 'FeatureCollection');
-  //     t.deepEqual(result.features[0].properties.name, 'New Jersey');
-  //     t.deepEqual(result.features[0].geometry, {
-  //         type: 'Point',
-  //         coordinates: [-74.38928604125977, 40.150275473401365]
-  //     });
-  //     t.end();
-  // });
-
   mbgeocode('./test/fixtures/data.csv', 'pk.eyJ1Ijoic3ZtYXR0aGV3cyIsImEiOiJVMUlUR0xrIn0.NweS_AttjswtN5wRuWCSNA', function(err, result) {
     t.ifError(err);
     console.log(result);
