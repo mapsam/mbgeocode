e5edea49a618de54602d05d6529c8d0678203d23
mapsam <matthews.sam@gmail.com>
2015-12-20 12:55:26 -0800

    update fixture data to cover test

--- index.js
+++ index.js
@@ -6,24 +6,15 @@ var fs = require('fs'),
     color = require('colors');
 
 module.exports = function(file, token, callback) {
-
-  // var parser = 
-  // });
-
   fs.readFile(file, 'utf8', function(err, csv) {
     if (err) throw err;
-
     parse(csv, {columns: true}, function(err, output) {
       if (err) throw err;
-
       geocode(output, token, function(err, geojson) {
         if (err) throw err;
-
         callback(null, geojson);
       });
     });
   });
-
-  // input.pipe(parser);
 };
 
